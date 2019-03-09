6598c5ba726cc99a8e86418815651058977928ac
Aashish kumar singh <41873953+ash-123-ish@users.noreply.github.com>
2019-03-09 23:14:07 +0530

    Update SUBSET.C

--- SUBSET.C
+++ SUBSET.C
@@ -2,7 +2,7 @@
 #include<conio.h>
 void main()
 {
-    int n,i,j,k,d,b[100],c[100],a[100][100],sum,x,y;
+    int n,i,j,k,b[100],c[100],a[100][100],sum,x,y;
     clrscr();
     scanf("%d",&n);
     for(i=0;i<n;i++)
@@ -16,14 +16,8 @@ void main()
 	for(k=0;k<c[i];k++)
 	{
 	    x=b[i]&a[i][k];
-	    for(d=0;d<c[i];d++)
-	    {
-		y=x&a[i][d];
-		if(y==0)
+		if(x==0)
 		sum++;
-	    }
-	    if(x==0)
-	    sum++;
 	}
 	if(sum>=1)
 	printf("Yes\n");
